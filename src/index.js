4f203143cf659f01fc118f721c9fc0b9a8b8c689
Rishu-aery <aeryrishab@gmail.com>
2025-05-30 08:14:49 +0530

    server created

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/serverConfig.js")
+
+const setupAndStartServer = async () => {
+
+    const app = express();
+
+    app.use(bodyParser.json);
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(() => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+}
+
+setupAndStartServer();
