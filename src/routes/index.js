7aab2955660d9ffc0d05d077c17f166b19a2799e
Rishabh aery <rishab.arrey@quodd.com>
2025-06-14 15:28:16 +0530

    basic routing setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const v1ApiRoutes = require("./v1/v1Routes.js");
 
-console.log("V1 Router----------", )
-
 router.use("/v1", v1ApiRoutes);
 
 module.exports = router; 
--- src/routes/v1/v1Routes.js
+++ src/routes/v1/v1Routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const CityController = require("../../controllers/cityController.js");
-console.log("city router-------");
+
 router.post("/city", CityController.create);
 
 module.exports = router;
