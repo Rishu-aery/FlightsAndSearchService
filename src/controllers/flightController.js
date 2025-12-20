3f954584347fe9ab53163a24d2dee7ae8cbe1dbd
Rishabh-aery <aeryrishab@gmail.com>
2025-12-21 00:45:46 +0530

    added error codes

--- src/controllers/flightController.js
+++ src/controllers/flightController.js
@@ -1,13 +1,24 @@
 const { FlightService } = require("../services/index.js");
+const { SuccessCodes } = require("../utils/errorCodes.js")
 
 const flightService = new FlightService();
 
 // POST -> /flight
 const create = async (req, res) => {
     try {
-        const body = req.body;
-        const flight = await flightService.createFlight(body);
-        res.status(201).json({
+        const body = req.body
+        const flightRequestData = {
+            flightNumber: body.flightNumber,
+            airplaneId: body.airplaneId,
+            departureAirportId: body.departureAirportId,
+            arrivalAirportId: body.arrivalAirportId,
+            arrivalTime: body.arrivalTime,
+            departureTime: body.departureTime,
+            price: body.price,
+            boardingGate: body.boardingGate
+        }
+        const flight = await flightService.createFlight(flightRequestData);
+        res.status(SuccessCodes.CREATED).json({
             data: flight,
             success: true,
             message: "Flight created successfully!",
@@ -28,7 +39,7 @@ const get = async (req, res) => {
     try {
         const { id } = req.params;
         const flight = await flightService.getFlightById(id);
-        res.status(200).json({
+        res.status(SuccessCodes.OK).json({
             data: flight,
             success: true,
         })
@@ -48,7 +59,7 @@ const getAll = async (req, res) => {
     try {
         const filter = req.query;
         const flight = await flightService.getAllFlights(filter);
-        res.status(200).json({
+        res.status(SuccessCodes.OK).json({
             data: flight,
             success: true,
         })
--- src/middlewares/flightMiddleware.js
+++ src/middlewares/flightMiddleware.js
@@ -1,8 +1,10 @@
+const { ClientErrors } = require("../utils/errorCodes.js")
+
 const validateCreateFlight = (req, res, next) => {
     const body = req.body;
 
     if (!body.flightNumber || !body.airplaneId || !body.departureAirportId || !body.arrivalAirportId || !body.arrivalTime || !body.departureTime || !body.price) {
-        return res.status(400).json({
+        return res.status(ClientErrors.BAD_REQUEST).json({
             data: {},
             success: false,
             message: "Invalid Request Body!",
